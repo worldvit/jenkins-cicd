f25ff2796aae7fb557983f8278afac292075633b
vagrant <vagrant@kvm-host>
2026-02-19 08:24:36 +0000

    Refactor to Express server with HTML page

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM node:18-alpine
 WORKDIR /app
+
+# 패키지 정보 파일을 먼저 복사하고 라이브러리 설치
+COPY package*.json ./
+RUN npm install
+
+# 나머지 진짜 코드들을 복사
 COPY . .
-# 3000번 포트를 열어두겠다는 명시적 선언
+
 EXPOSE 3000
-# 서버 실행 명령어
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('🚀 Jenkins CI/CD 파이프라인 완벽 구축 성공! 24시간 멈추지 않는 서버입니다.\n');
-});
 
-server.listen(port, () => {
-  console.log(`서버가 ${port}번 포트에서 가동 중입니다...`);
+// 'public' 폴더 안에 있는 정적 파일(HTML, CSS, 이미지 등)을 서비스하겠다고 선언
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`🚀 Express 웹 서버가 ${port}번 포트에서 가동 중입니다...`);
 });
